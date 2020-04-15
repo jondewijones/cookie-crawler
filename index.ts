4e2fcc0b7e175323a1a4a7027157e49e8c93cb72
janderson2 <j.anderson.ons@gmail.com>
2020-04-15 10:50:40 +0100

    Hack around to support larger numbers of URLs
    This is very rough, but seems to be working.

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import puppeteer from "puppeteer";
 import 'console.table';
 import { Cookie } from "./models";
-import HTML from "./html"
+import HTML from "./html";
+import PQueue from "p-queue";
 
 const URLs: string[] = [
     "https://www.ons.gov.uk/visualisations/dvc368/dashboard/index.html",
@@ -9,26 +10,29 @@ const URLs: string[] = [
 
 ]
 
-const getCookiesFromPage = async() => {
-    return new Promise(async(resolve, reject) => {
+const queue = new PQueue({ concurrency: 10 });
+
+const getCookiesFromPage = async () => {
+    return new Promise(async (resolve, reject) => {
         const cookieGets = URLs.map((url, index) => {
-            return new Promise(async(resolve, reject) => {
+            return queue.add(async () => {
                 try {
                     const browser = await puppeteer.launch();
                     const page = await browser.newPage();
                     console.log(`navigating to ${url} ...`)
                     await page.goto(url);
                     //await page.screenshot({path: index+'.png'});
-                    const data = await page._client.send('Network.getAllCookies'); 
+                    const data = await (page as any)._client.send('Network.getAllCookies');
                     await browser.close();
-                    resolve(data.cookies)
-                }catch(error) {
+                    return data.cookies
+                } catch (error) {
                     console.error(error)
                     reject(error)
                 }
-            })
-        })
-        await Promise.all(cookieGets).then(result => {resolve(result)}).catch((error)=>{reject(error)})     
+            });
+        });
+        await queue.onEmpty();
+        await Promise.all(cookieGets).then(result => { resolve(result) }).catch((error) => { reject(error) })
     })
 }
 
@@ -40,7 +44,7 @@ const arrFlatten = (arr: any): Array<Cookie> => {
 //     return [...new Set(arr)]
 // }
 
-(async ()=> {
+(async () => {
     const allCookies = await getCookiesFromPage().then(result => {
         return arrFlatten(result)
     })
